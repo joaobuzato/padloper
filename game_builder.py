9ae38040f7b2ff53accba1a86317e4a327ddb864
Buzato <joaobuzato@gmail.com>
2022-10-06 16:19:24 -0300

    fixing game_builder

--- game_builder.py
+++ game_builder.py
@@ -1,13 +1,20 @@
 import json
 
 # json loads // json dumps
+class GameBuilder:
+    def __init__(self):
+        self.game_map = json.loads(open('map.json').read())
+        self.pad_main_txt = ""
+        self.build_game()
 
-game_map = json.loads(open('map.json').read())
+    def build_game(self):
+        self.create_screen()
+        self.create_actors()
+        self.create_main()
+    def create_screen(self):
+        screen = self.game_map.get("screen")
 
-print(game_map)
-screen = game_map.get("screen")
-pad_main_txt = ""
-screen_txt = f""" 
+        screen_txt = f""" 
 from turtle import Screen
 
 class PadScreen():
@@ -15,22 +22,23 @@ class PadScreen():
     def __init__(self):
         self.screen = Screen()
         self.screen.setup(width={screen.get('width')}, height={screen.get('height')})
-        self.screen.title('{game_map.get('name')}')
+        self.screen.title('{self.game_map.get('name')}')
 
     def get_screen_obj(self):
         return self.screen
 """
-file = open("src/padscreen.py", "w")
-file.write(screen_txt)
-file.close()
+        file = open("src/padscreen.py", "w")
+        file.write(screen_txt)
+        file.close()
 
-manager_setups = ""
-input_setups = ""
-for actor in game_map.get("actors"):
-    class_name = str.title(actor.get("name"))
-    pad_main_txt += f"""
+    def create_actors(self):
+        self.manager_setups = ""
+        self.input_setups = ""
+        for actor in self.game_map.get("actors"):
+            class_name = str.title(actor.get("name"))
+            self.pad_main_txt += f"""
 from {actor.get("name")}_manager import {class_name}Manager"""
-    actor_txt = f"""
+            actor_txt = f"""
 from turtle import Turtle
 
 class {class_name} (Turtle):
@@ -42,11 +50,11 @@ class {class_name} (Turtle):
         self.color('{actor.get("components").get("color")}')
 """
 
-    file = open(f"src/{actor.get('name')}.py", "w")
-    file.write(actor_txt)
-    file.close()
+            file = open(f"src/{actor.get('name')}.py", "w")
+            file.write(actor_txt)
+            file.close()
 
-    manager_txt = f"""
+            self.manager_txt = f"""
 from {actor.get("name")} import {class_name}
 
 class {class_name}Manager():
@@ -58,32 +66,32 @@ class {class_name}Manager():
         
     def input(self):
 """
-    if actor.get("behaviors").get("inputs") != None:
-        functions_txt = ""
-        for input in actor.get("behaviors").get("inputs"):
-            manager_txt += f"""
+            if actor.get("behaviors").get("inputs") != None:
+                functions_txt = ""
+                for input in actor.get("behaviors").get("inputs"):
+                    self.manager_txt += f"""
         self.screen.onkey(key='{input.get('key')}', fun=self.{input.get('key')})"""
-            functions_txt += f"""
+                    functions_txt += f"""
     def {input.get("key")}(self):
         self.actor.{input.get("action")}({input.get("param")})
 """
 
-        manager_txt += functions_txt
+                self.manager_txt += functions_txt
 
-    manager_txt += """
+            self.manager_txt += """
         pass
 """
 
-    manager_setups += f"""
+            self.manager_setups += f"""
         self.{actor.get("name")}_manager = {class_name}Manager(self.screen)"""
-    input_setups += f"""
+            self.input_setups += f"""
         self.{actor.get("name")}_manager.input()"""
-    file = open(f"src/{actor.get('name')}_manager.py", "w")
-    file.write(manager_txt)
-    file.close()
+            file = open(f"src/{actor.get('name')}_manager.py", "w")
+            file.write(self.manager_txt)
+            file.close()
 
-
-pad_main_txt += f"""
+    def create_main(self):
+        self.pad_main_txt += f"""
 import time
 from padscreen import PadScreen
 
@@ -92,10 +100,10 @@ class PadMain():
     def __init__(self):
         padscreen = PadScreen()
         self.screen = padscreen.get_screen_obj()
-        {manager_setups}
+        {self.manager_setups}
     
     def input(self):
-        {input_setups}
+        {self.input_setups}
         pass
     
     def update(self):
@@ -118,7 +126,10 @@ pad_main = PadMain()
 pad_main.game_loop()
 """
 
-file = open("src/padmain.py", "w")
-file.write(pad_main_txt)
-file.close()
+        file = open("src/padmain.py", "w")
+        file.write(self.pad_main_txt)
+        file.close()
+
+
+GameBuilder()
 
