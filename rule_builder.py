8a6e67d0c2ddca5f7ea2f701557bc9e6e576f6e4
Buzato <joaobuzato@gmail.com>
2022-11-09 19:52:28 -0300

    adicionando regras para jogo vencido e para jogador atingir uma certa pontuação

--- rule_builder.py
+++ rule_builder.py
@@ -27,30 +27,37 @@ class RuleBuilder:
         obj = self.{rule.get("actor")}_manager.check_position(y_pos={y_pos},x_pos={x_pos}, y_cond='{y_cond}',x_cond='{x_cond}')
         if obj.get("position_checked"):
                 """
-
-
-
+            
+            if rule.get("trigger") == "score" :
+                win_score = rule.get("win_score")
+                self.rules_setups += f"""
+        if self.scoreboard.score >= {win_score} :    
+                """
 
 
             for consequence in rule.get('consequences'):
                 if consequence.get("name") == "print":
                     self.rules_setups += f"""
                 print("collision!")
-            """
+                """
+                elif consequence.get("name") == "game_won":
+                    self.rules_setups += f"""
+                self.scoreboard.game_won()
+                """
                 elif consequence.get("name") == "point":
                     self.rules_setups += f"""
                 self.scoreboard.point()  
-            """
+                """
                 elif consequence.get("name") == "game_over":
                     self.rules_setups += f"""
                 self.scoreboard.game_over()
                 self.game_is_on = False
-            """
+                """
                 elif consequence.get("name") == "move_to":
                     self.rules_setups += f"""
                 obj.get("actor").goto({consequence.get("x"), consequence.get("y")})
-                    """
+                """
                 else:
                     self.rules_setups += f"""
                 pass
-            """
+                """
